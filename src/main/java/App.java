b33467e5a2bbf1df570daeaf8475fa467a2cb07f
pavel lazarchuk <ogheat1337@gmail.com>
2024-05-12 17:32:22 +0300

    initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println("HelloWorld result singleton Bean" + " " + (bean==bean1));
+        System.out.println("Cat result prototype Bean" + " " + (cat==cat1));
+
+
+
     }
 }
